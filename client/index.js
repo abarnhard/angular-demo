7e2b04f021c2d87e94d6ccb03a920983033beb36
Adam Barnhard <admbarn@gmail.com>
2014-09-08 12:54:27 -0500

    finished demo

---
+++ client/index.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  angular.module('demo', [])
+  .controller('MainController', ['$scope', function($scope){
+    $scope.name = 'Groot!';
+    $scope.age = 47;
+    $scope.dogs = [
+      {name:'fido', age:3},
+      {name:'baxter', age:5},
+      {name:'fluffy', age:2}
+    ];
+  }]);
+
+})();
